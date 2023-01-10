53fa9d64b504ff82a782d1706b16cd5c7d3d0a8f
Aleksandra-Destro <121759568+Aleksandra-Destro@users.noreply.github.com>
2023-01-10 14:00:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+const img1 = document.querySelector("#img1");
+
+img1.addEventListener("mouseover", function (e) {
+  document.getElementById("img1").src =
+    "../Pictures/Replacing/peony-bouquet.jpg";
+});
+
+img1.addEventListener("mouseout", function (e) {
+  document.getElementById("img1").src = "../Pictures/bouquet-2.jpg";
+});
+
+const img2 = document.querySelector("#img2");
+
+img2.addEventListener("mouseover", function (e) {
+  document.getElementById("img2").src = "../Pictures/Replacing/table-5.jpg";
+});
+
+img2.addEventListener("mouseout", function (e) {
+  document.getElementById("img2").src = "../Pictures/table-1.jpg";
+});
+
+const img3 = document.querySelector("#img3");
+
+img3.addEventListener("mouseover", function (e) {
+  document.getElementById("img3").src = "../Pictures/Replacing/working-1.jpg";
+});
+
+img3.addEventListener("mouseout", function (e) {
+  document.getElementById("img3").src = "../Pictures/arch-2.jpg";
+});
+
+const img4 = document.querySelector("#img4");
+
+img4.addEventListener("mouseover", function (e) {
+  document.getElementById("img4").src = "../Pictures/Replacing/bride-4.jpg";
+});
+
+img4.addEventListener("mouseout", function (e) {
+  document.getElementById("img4").src = "../Pictures/bride-2.jpg";
+});
+
+const img5 = document.querySelector("#img5");
+
+img5.addEventListener("mouseover", function (e) {
+  document.getElementById("img5").src = "../Pictures/Replacing/cake-3.jpg";
+});
+
+img5.addEventListener("mouseout", function (e) {
+  document.getElementById("img5").src = "../Pictures/cake-1.jpg";
+});
+
+const img6 = document.querySelector("#img6");
+
+img6.addEventListener("mouseover", function (e) {
+  document.getElementById("img6").src = "../Pictures/Replacing/working-4.jpg";
+});
+
+img6.addEventListener("mouseout", function (e) {
+  document.getElementById("img6").src = "../Pictures/suitcase-flowers.jpg";
+});
+
+const img7 = document.querySelector("#img7");
+
+img7.addEventListener("mouseover", function (e) {
+  document.getElementById("img7").src = "../Pictures/Replacing/vase-1.jpg";
+});
+
+img7.addEventListener("mouseout", function (e) {
+  document.getElementById("img7").src = "../Pictures/table-4.jpg";
+});
+
+const img8 = document.querySelector("#img8");
+
+img8.addEventListener("mouseover", function (e) {
+  document.getElementById("img8").src = "../Pictures/Replacing/table-2.jpg";
+});
+
+img8.addEventListener("mouseout", function (e) {
+  document.getElementById("img8").src = "../Pictures/vase-4.jpg";
+});
+
+const img9 = document.querySelector("#img9");
+
+img9.addEventListener("mouseover", function (e) {
+  document.getElementById("img9").src = "../Pictures/Replacing/working-2.jpg";
+});
+
+img9.addEventListener("mouseout", function (e) {
+  document.getElementById("img9").src = "../Pictures/working-5.jpg";
+});
